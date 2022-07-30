0191a0fe3499342d4e05281e623b140fa971166f
JVitor96 <89887933+JVitor96@users.noreply.github.com>
2022-07-29 21:27:00 -0300

    adição de arquivos via upload

---
+++ Hellogit/src/main/java/br/vom/jvitor96/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.vom.jvitor96.git;
+
+/**
+ * @author so_ga
+ * Integrando o Eclipse ao github
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
